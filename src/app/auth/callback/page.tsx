c20e77eab0b4c6e80614959aa3a85fe8d053e499
sai <saiprasanna1003@gmail.com>
2026-02-14 21:29:43 +0530

    Callback file issue fix

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,58 +1,7 @@
-"use client";
+import CallbackClient from "./callbackClient";
 
 export const dynamic = "force-dynamic";
 
-import { useEffect } from "react";
-import { supabase } from "@/lib/supabase";
-import { useRouter, useSearchParams } from "next/navigation";
-
-export default function Callback() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const mode = searchParams.get("mode");
-
-  useEffect(() => {
-    const handleAuth = async () => {
-      const { data } = await supabase.auth.getUser();
-      const user = data.user;
-
-      if (!user) return;
-
-      const { data: profile } = await supabase
-        .from("profiles")
-        .select("*")
-        .eq("id", user.id)
-        .single();
-
-      // SIGNUP FLOW
-      if (mode === "signup") {
-        if (profile) {
-          alert("Already registered. Please login.");
-          router.push("/");
-        } else {
-          await supabase.from("profiles").insert({
-            id: user.id,
-            email: user.email,
-          });
-
-          alert("Registration successful! You can now access dashboard.");
-          router.push("/dashboard");
-        }
-      }
-
-      // LOGIN FLOW
-      if (mode === "login") {
-        if (!profile) {
-          alert("User not found. Please sign up first.");
-          router.push("/");
-        } else {
-          router.push("/dashboard");
-        }
-      }
-    };
-
-    handleAuth();
-  }, [mode, router]);
-
-  return <p className="text-center mt-10">Processing...</p>;
+export default function Page() {
+  return <CallbackClient />;
 }
